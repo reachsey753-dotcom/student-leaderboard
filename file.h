0e34a74815a9b76f1b3f070801b9331a2db38195
Hongsong07 <ht6026010046@camtech.edu.kh>
2026-08-28 15:21:21 +0700

    Create file.h

---
+++ file.h
@@ -0,0 +1,29 @@
+#ifndef FILE.H
+#define FILE .H
+
+#define MAX_NAME 50
+
+typedef struct
+{
+    int id;
+    char name[MAX_NAME];
+    int score;
+    int missions;
+} Team;
+
+extern Team *team;
+extern int size;
+extern int capacity;
+
+int findteambyIndex(int id);
+int ensuredCapacity(int required);
+int addTeam(int id, const char *name, int score, int missions);
+int recordMission(int id, int points);
+int deleteTeam(int id);
+void sortLeaderboard();
+void displayTeam();
+void loadTeams(const char *filename);
+int saveTeams(const char *filename);
+void cleanupTeams(void);
+
+#endif
